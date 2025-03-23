9cd44e2560c10291ff9fa48e22e045643584d872
IMODIL <mdil181296@gmail.com>
2025-03-23 22:22:23 +0700

    edit quest-center

---
+++ app/components/dropDown.js
@@ -0,0 +1,54 @@
+"use client";
+import { useState } from "react";
+
+export default function Dropdown({ items, onSelect, placeholder }) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const filteredItems = items.filter((item) =>
+    item.item_name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const handleSelect = (item) => {
+    setSelectedItem(item); // Simpan item yang dipilih
+    setSearchTerm(""); // Reset searchTerm
+    setIsOpen(false); // Tutup dropdown
+    onSelect(item); // Kirim item yang dipilih ke parent component
+  };
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value); // Perbarui searchTerm
+    if (selectedItem) {
+      setSelectedItem(null); // Reset selectedItem jika ada
+    }
+  };
+
+  return (
+    <div className="relative">
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={selectedItem ? selectedItem.item_name : searchTerm}
+        onChange={handleInputChange}
+        onFocus={() => setIsOpen(true)}
+        onBlur={() => setTimeout(() => setIsOpen(false), 200)}
+        className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+
+      {isOpen && (
+        <div className="absolute z-10 w-full mt-2 bg-white border border-gray-300 rounded-md shadow-lg max-h-60 overflow-y-auto">
+          {filteredItems.map((item) => (
+            <div
+              key={item.shortname}
+              onClick={() => handleSelect(item)}
+              className="p-2 hover:bg-gray-100 cursor-pointer"
+            >
+              {item.item_name}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
--- app/quest-center/page.js
+++ app/quest-center/page.js
@@ -1,7 +1,116 @@
-export default function QuestCenter() {
+"use client";
+import { useState } from "react";
+import DropDown from "../components/dropDown";
+import quest from "./quest.json";
+
+export default function QuestCenter({}) {
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [currentFame, setCurrentFame] = useState(0);
+  const [targetFame, setTargetFame] = useState(0);
+  const [result, setResult] = useState(null);
+
+  const handleSelect = (item) => {
+    setSelectedItem(item);
+    console.log(item);
+  };
+
+  const handleCurrentFameChange = (e) => {
+    const val = parseInt(e.target.value);
+    setCurrentFame(val >= 0 ? val : 0);
+  };
+
+  const handleTargetFameChange = (e) => {
+    const val = parseInt(e.target.value);
+    setTargetFame(val >= 0 ? val : 0);
+  };
+
+  const handleSub = () => {
+    if (!selectedItem) {
+      alert("Pilih Item Quest terlebih dahulu!");
+      return;
+    }
+
+    //math
+    const fneed = targetFame - currentFame;
+    if (fneed <= 0) {
+      alert("Target fame harus lebih besar dari fame sekarang!");
+      return;
+    }
+    const totQ = Math.ceil(fneed / selectedItem.fame);
+    const totFd = totQ * selectedItem.fame;
+    const totCeg = totQ * selectedItem.cegel;
+    const totIt = totQ * selectedItem.quantity;
+    //
+    const sS = 300;
+    const totS = Math.floor(totIt / sS);
+    const sisa = totIt % sS;
+
+    setResult({
+      totFd,
+      totCeg,
+      totIt,
+      totS,
+      sisa,
+    });
+  };
+
   return (
-    <div className="flex mx-auto">
-      <h2>Test ini Center</h2>
+    <div className="h-auto w-3/4 shadow-lg p-10 mx-auto rounded">
+      <h2>Pilih Quest</h2>
+      <DropDown
+        items={quest}
+        onSelect={handleSelect}
+        placeholder="Search items ..."
+      />
+
+      {selectedItem && (
+        <div className="mt-4">
+          <div className="mb-4">
+            <h2>Fame Sekarang</h2>
+            <input
+              type="number"
+              placeholder="Masukkan Fame Sekarang"
+              value={currentFame}
+              onChange={handleCurrentFameChange}
+              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          {/* ========== */}
+          <div className="mb-4">
+            <h2>Fame Sekarang</h2>
+            <input
+              type="number"
+              placeholder="Masukkan Fame Sekarang"
+              value={targetFame}
+              onChange={handleTargetFameChange}
+              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <button
+            onClick={handleSub}
+            className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 cursor-pointer"
+          >
+            Submit
+          </button>
+        </div>
+      )}
+
+      {result && (
+        <div className="mt-4 p-4 bg-gray-100 rounded-md">
+          <h3 className="text-lg font-semibold">Hasil Perhitungan:</h3>
+          <p>
+            Banyak item yang dibutuhkan: {result.totIt}
+            {result.totS > 0
+              ? ` (${result.totS} stak${
+                  result.sisa > 0 ? `, ${result.sisa} pcs` : ""
+                })`
+              : ""}
+          </p>
+          <p>Jumlah fame yang didapat: {result.totFd}</p>
+          <p>Jumlah cegel yang didapat: {result.totCeg}</p>
+        </div>
+      )}
     </div>
   );
 }
