3841e9aa8dd874108850037be549b421876f4a3f
IMODIL <mdil181296@gmail.com>
2025-03-24 04:40:23 +0700

    change font

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,10 @@
 import "./globals.css";
 import Sidebar from "./components/sideBar";
-import { Roboto } from "@next/font/google";
-
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`flex bg-white ${roboto.className}`}>
+      <body className="flex bg-white ">
         <Sidebar />
         <div className="flex-1 p-8 bg-white">{children}</div>
       </body>
